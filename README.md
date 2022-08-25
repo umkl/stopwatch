fe072ec53a86d2a582e18915b9f8316d9b5d0dbc
Michael Ungar <michael.ungar03@gmail.com>
2022-08-25 09:00:54 +0200

    fix: timer + feat: readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# stopwatch
 
-Lets you start and stop a timer.
+<style type="text/css">
+    img {
+        width: 100px;
+
+    }
+</style>
+
+![stopwatch](./assets/command-icon.png)
+# stopwatch
+## why?
+In order to track the time it would be handy if you had a little stopwatch at hand I thought. Because raycast is _the tool_ for being handy I thought I would develop a extension for it.
+## Get started
+Install the extension and enter start or stop stopwatch. After stopping the stopwatch the time will be automatically put into your clipboard so you can quickly use it.
+
--- src/stopwatchUtils.ts
+++ src/stopwatchUtils.ts
@@ -1,5 +1,6 @@
 import { Clipboard, environment, showHUD } from "@raycast/api";
-import { existsSync, readdirSync, readFileSync, writeFileSync } from "fs";
+import { existsSync, readdirSync, readFileSync, writeFileSync, unlinkSync } from "fs";
+import moment from "moment";
 import { extname } from "path";
 
 async function createAndStartStopwatch(stopwatchName = "Stopwatch") {
@@ -13,6 +14,7 @@ async function stopAndCopyStopwatch() {
   // await showHUD("Time elapsed: " + (new Date().getTime() - starttime) + "ms");
 
   const files = readdirSync(environment.supportPath);
+  showHUD(files.length + " stopwatches found");
   files.forEach((stopwatchFile: string) => {
     if (extname(stopwatchFile) == ".timer") {
       const stopwatchName = readFileSync(environment.supportPath + "/" + stopwatchFile).toString();
@@ -20,8 +22,13 @@ async function stopAndCopyStopwatch() {
       console.log(stopwatchFileParts[0]);
       let now = new Date().getTime();
       let timeElapsed = new Date(now - parseInt(stopwatchFileParts[1]));
-      showHUD("Time elapsed: " + timeElapsed.getSeconds() + " seconds");
-      Clipboard.copy(timeElapsed.getSeconds().toString());
+      let ahour = new Date();
+      ahour.setHours(1);
+      // let subtractedTime = new Date(ahour.getTime() - timeElapsed.getTime());
+      const elapsedFormatted = moment(timeElapsed.getTime()).format("HH:mm:ss");
+      showHUD("Time elapsed: " + elapsedFormatted);
+      Clipboard.copy(elapsedFormatted);
+      unlinkSync(`${environment.supportPath}/${stopwatchFile}`);
     }
   });
 }
